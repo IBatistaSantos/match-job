d4112d57389caa4e4b4f4e5ddde330def4436524
Israel Batista <israelbatiista19@gmail.com>
2024-12-03 18:26:51 -0300

    feat: create config  of dependencies candidate module

---
+++ src/candidate/candidate.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CandidateDocument,
+  CandidateSchema,
+} from './repositories/implementations/mongo/candidate.schema';
+import { CreateCandidateService } from './services/create-candidate.service';
+import { MongoCandidateRepository } from './repositories/implementations/mongo/mongo-candidate.repository';
+import { OpenAIEmbed } from './providers/embed/implementations/openai-embed.provider';
+import { PineconeProvider } from './providers/search/implementations/pinecone.provider';
+import { CandidateController } from './controllers/candidate.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: CandidateDocument.name,
+        schema: CandidateSchema,
+      },
+    ]),
+  ],
+  controllers: [CandidateController],
+  providers: [
+    CreateCandidateService,
+    {
+      provide: 'CandidateRepository',
+      useClass: MongoCandidateRepository,
+    },
+    {
+      provide: 'EmbeddingProvider',
+      useClass: OpenAIEmbed,
+    },
+    {
+      provide: 'SearchProvider',
+      useClass: PineconeProvider,
+    },
+  ],
+})
+export class CandidateModule {}
