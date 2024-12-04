d2d36ffffe4a4d89f95d31d7debdbc68eb41ea0c
Israel Batista <israelbatiista19@gmail.com>
2024-12-04 19:29:19 -0300

    feat: create job module

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -7,17 +7,45 @@ import {
 import { CreateJobService } from './services/create-job.service';
 import { MongoJobRepository } from './repositories/implementations/mongo-job.repository';
 import { JobController } from './controllers/job.controller';
+import {
+  CandidateDocument,
+  CandidateSchema,
+} from '@/candidate/repositories/implementations/mongo/candidate.schema';
+import { FindMatchCandidateService } from './services/find-match-candidate.service';
+import { OpenAIEmbed } from '@/shared/providers/embed/implementations/openai-embed.provider';
+import { PineconeProvider } from '@/shared/providers/search/implementations/pinecone.provider';
+import { DeleteJobService } from './services/delete.job.service';
+import { UpdateJobService } from './services/update-job.service';
+import { DetailJobService } from './services/details-job.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: JobStructure.name, schema: JobSchema }]),
+    MongooseModule.forFeature([
+      { name: JobStructure.name, schema: JobSchema },
+      {
+        name: CandidateDocument.name,
+        schema: CandidateSchema,
+      },
+    ]),
   ],
   controllers: [JobController],
   providers: [
     CreateJobService,
+    DeleteJobService,
+    UpdateJobService,
+    DetailJobService,
+    FindMatchCandidateService,
     {
       provide: 'JobRepository',
       useClass: MongoJobRepository,
+    },
+    {
+      provide: 'EmbeddingProvider',
+      useClass: OpenAIEmbed,
+    },
+    {
+      provide: 'SearchProvider',
+      useClass: PineconeProvider,
     },
   ],
 })
