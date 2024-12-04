1089c64ac91c50009ce560bdb9dd82c45a08a80d
Israel Batista <israelbatiista19@gmail.com>
2024-12-04 19:32:00 -0300

    feat: create method and implementations job repository

--- src/job/repositories/implementations/mongo-job.repository.ts
+++ src/job/repositories/implementations/mongo-job.repository.ts
@@ -4,14 +4,52 @@ import { Model, Types } from 'mongoose';
 
 import { Job } from '@/job/entities/job.entity';
 import { JobRepository } from '../job.repository';
-import { JobDocument, JobStructure } from './job.schema';
+import { JobStructure } from './job.schema';
+import { Candidate } from '@/candidate/entities/candidate';
+import { CandidateDocument } from '@/candidate/repositories/implementations/mongo/candidate.schema';
 
 @Injectable()
 export class MongoJobRepository implements JobRepository {
   constructor(
     @InjectModel(JobStructure.name)
-    private jobModel: Model<JobDocument>,
+    private jobModel: Model<JobStructure>,
+
+    @InjectModel(CandidateDocument.name)
+    private candidateModel: Model<CandidateDocument>,
   ) {}
+
+  async findById(id: string): Promise<Job | null> {
+    const job = await this.jobModel
+      .findOne({ _id: new Types.ObjectId(id), status: 'ACTIVE' })
+      .exec();
+    return job
+      ? new Job({
+          id: job._id.toString(),
+          createdAt: job.createdAt,
+          updatedAt: job.updatedAt,
+          description: job.description,
+          title: job.title,
+          skills: job.skills,
+        })
+      : null;
+  }
+  async listCandidates(candidateIds: string[]): Promise<Candidate[]> {
+    const candidates = await this.candidateModel
+      .find({ _id: { $in: candidateIds }, status: 'ACTIVE' })
+      .exec();
+    return candidates.map(
+      (candidate) =>
+        new Candidate({
+          id: candidate._id.toString(),
+          createdAt: candidate.createdAt,
+          updatedAt: candidate.updatedAt,
+          name: candidate.name,
+          email: candidate.email,
+          skills: candidate.skills,
+          resume: candidate.resume,
+        }),
+    );
+  }
   async save(job: Job): Promise<void> {
     const jobJSON = job.toJSON();
     const { id, ...rest } = jobJSON;
@@ -19,5 +57,11 @@ export class MongoJobRepository implements JobRepository {
       _id: new Types.ObjectId(id),
       ...rest,
     });
+  }
+
+  async update(job: Job): Promise<void> {
+    const jobJSON = job.toJSON();
+    const { id, ...rest } = jobJSON;
+    await this.jobModel.updateOne({ _id: new Types.ObjectId(id) }, rest).exec();
   }
 }
--- src/job/repositories/job.repository.ts
+++ src/job/repositories/job.repository.ts
@@ -1,5 +1,9 @@
+import { Candidate } from '@/candidate/entities/candidate';
 import { Job } from '../entities/job.entity';
 
 export interface JobRepository {
+  findById(id: string): Promise<Job | null>;
+  listCandidates(candidateIds: string[]): Promise<Candidate[]>;
   save(job: Job): Promise<void>;
+  update(job: Job): Promise<void>;
 }
