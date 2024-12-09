e02c225aebc90ea869b5bc320e6acaafd1e16f18
Israel Batista <israelbatiista19@gmail.com>
2024-12-09 12:15:31 -0300

    feat: create candidate feature

--- src/candidate/services/create-candidate.service.ts
+++ src/candidate/services/create-candidate.service.ts
@@ -1,12 +1,14 @@
 import { BadRequestException, Injectable, Inject } from '@nestjs/common';
 import { Candidate } from '../entities/candidate';
 import { EmbeddingProvider } from '../../shared/providers/embed/embed.provider';
-import { SearchProvider } from '../../shared/providers/search/search.provider';
+import { VectorialDatabase } from '../../shared/providers/vectorial-database/vectorial-database.provider';
 import { CandidateRepository } from '../repositories/candidate.repository';
+import { HashProvider } from '@/shared/providers/hash/hash.provider';
 
 interface CreateCandidateInput {
   name: string;
   email: string;
+  password: string;
   skills: string[];
   resume: string;
 }
@@ -18,8 +20,10 @@ export class CreateCandidateService {
     private readonly repository: CandidateRepository,
     @Inject('EmbeddingProvider')
     private readonly embeddingProvider: EmbeddingProvider,
-    @Inject('SearchProvider')
-    private readonly searchProvider: SearchProvider,
+    @Inject('VectorialDatabase')
+    private readonly vectorialDB: VectorialDatabase,
+    @Inject('HashProvider')
+    private readonly hashProvider: HashProvider,
   ) {}
   async execute(params: CreateCandidateInput): Promise<{ id: string }> {
     const existingCandidate = await this.repository.findByEmail(params.email);
@@ -28,7 +32,15 @@ export class CreateCandidateService {
       throw new BadRequestException('Candidate with this email already exists');
     }
 
-    const candidate = new Candidate(params);
+    const hashedPassword = await this.hashProvider.hash(params.password);
+
+    const candidate = new Candidate({
+      name: params.name,
+      email: params.email,
+      password: hashedPassword,
+      skills: params.skills,
+      resume: params.resume,
+    });
 
     const embeddedResume = await this.embeddingProvider.embed(candidate.resume);
 
@@ -36,7 +48,7 @@ export class CreateCandidateService {
       throw new BadRequestException('Resume embedding failed');
     }
 
-    await this.searchProvider.index(candidate.id, embeddedResume);
+    await this.vectorialDB.index(candidate.id, embeddedResume);
 
     await this.repository.save(candidate);
 
