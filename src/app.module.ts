6ea025e06deecaa45a0aad61c136ac6bdc116c81
Israel Batista <israelbatiista19@gmail.com>
2024-12-09 13:33:35 -0300

    feat: add jwt module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { CandidateModule } from './candidate/candidate.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JobModule } from './job/job.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -12,6 +13,13 @@ import { JobModule } from './job/job.module';
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
       }),
       inject: [ConfigService],
     }),
